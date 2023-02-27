c2235a3bee505153bb4070593f4dda0cdaea79fc
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-02-27 13:05:35 +0530

    Update mealy.v

--- mealy/mealy111000/mealy.v
+++ mealy/mealy111000/mealy.v
@@ -15,63 +15,63 @@ always @(cst or din)
  case (cst)
    S0: if (din == 1'b1)
           begin
-         nst = S1;
-          y=1'b0;
+         nst <= S1;
+          y<=1'b0;
           end
       else
           begin
-           nst = cst;
-          y=1'b0;
+           nst <= cst;
+          y<=1'b0;
           end
    S1: if (din == 1'b1)
           begin
-        nst = S2;
-          y=1'b0;
+        nst <= S2;
+          y<=1'b0;
           end
        else
           begin
-           nst = S0;
-          y=1'b0;
+           nst <= S0;
+          y<=1'b0;
           end
    S2: if (din == 1'b1)
           begin
-         nst = S3;
-          y=1'b0;
+         nst <= S3;
+          y<=1'b0;
           end    
             else
           begin
-           nst = S0;
-          y=1'b0;
+           nst <= S0;
+          y<=1'b0;
           end
    S3: if (din == 1'b0)
           begin
-         nst = S4;
-          y=1'b0;
+         nst <= S4;
+          y<=1'b0;
           end
        else
           begin
-          nst = cst;
-          y=1'b0;
+          nst <= cst;
+          y<=1'b0;
           end
    S4: if (din == 1'b0)
           begin
-         nst = S5;
-          y=1'b0;
+         nst <= S5;
+          y<=1'b0;
           end
        else
           begin
-          nst = S1;
-          y=1'b0;
+          nst <= S1;
+          y<=1'b0;
           end
     S5: if (din == 1'b0)
           begin
-         nst = S0;
-          y=1'b1;
+         nst <= S0;
+          y<=1'b1;
           end
        else
           begin
-          nst = S1;
-          y=1'b0;
+          nst <= S1;
+          y<=1'b0;
           end
    default: nst = S0;
   endcase
